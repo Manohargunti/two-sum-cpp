fdafe6674a76d9cec2b5c12ee4fc6cf2c77d04dd
MS <manohargunti2023@gmail.com>
2025-08-30 15:54:41 -0500

    Add main.cpp with Two Sum solution
    Implemented C++ solution using unordered_map to achieve O(n) time complexity.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int> mp;  // stores number -> index
     for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i];
+        int complement = target - nums[i]; 
         if (mp.find(complement) != mp.end()) {
             return {mp[complement], i};
         }
