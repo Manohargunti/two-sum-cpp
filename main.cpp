e2883710664e874cfab2cd2f646d0cc9c97ca970
MS <manohargunti2023@gmail.com>
2025-08-30 16:00:06 -0500

    Add main.cpp with Two Sum solution
    Implemented C++ solution using unordered_map to achieve O(n) time complexity.

--- main.cpp
+++ main.cpp
@@ -4,29 +4,39 @@
 using namespace std;
 
 vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int, int> mp;  // stores number -> index
+    unordered_map<int, int> numMap;
     for (int i = 0; i < nums.size(); i++) {
-        int complement = target - nums[i]; 
-        if (mp.find(complement) != mp.end()) {
-            return {mp[complement], i};
+        int complement = target - nums[i];
+        if (numMap.find(complement) != numMap.end()) {
+            return {numMap[complement], i};
         }
-        mp[nums[i]] = i;
+        numMap[nums[i]] = i;
     }
-    return {}; // per problem there is always one valid answer
+    return {};
 }
 
 int main() {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 9;
+    int n, target;
+
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    if (n < 2 || n > 10000) {
+        cout << "Invalid size. Please enter a number between 2 and 10000.";
+        return 0;
+    }
+
+    vector<int> nums(n);
+    cout << "Enter " << n << " numbers: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Enter target: ";
+    cin >> target;
 
     vector<int> result = twoSum(nums, target);
-
-    cout << "[";
-    for (int i = 0; i < result.size(); i++) {
-        cout << result[i];
-        if (i != result.size() - 1) cout << ",";
-    }
-    cout << "]" << endl;
+    cout << "Output: [" << result[0] << "," << result[1] << "]";
 
     return 0;
 }
