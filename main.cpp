d7d18432fad8bcd97c827f4678ed251023fa749e
MS <manohargunti2023@gmail.com>
2025-08-30 15:50:20 -0500

    Create main.cpp
    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;  // stores number -> index
+    for (int i = 0; i < nums.size(); i++) {
+        int complement = target - nums[i];
+        if (mp.find(complement) != mp.end()) {
+            return {mp[complement], i};
+        }
+        mp[nums[i]] = i;
+    }
+    return {}; // per problem there is always one valid answer
+}
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = twoSum(nums, target);
+
+    cout << "[";
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i];
+        if (i != result.size() - 1) cout << ",";
+    }
+    cout << "]" << endl;
+
+    return 0;
+}
